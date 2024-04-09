60435b8b2544919ec099ab9429ecb060131f8c76
ThStranick <ThStranick@gmail.com>
2024-04-09 11:45:54 -0400

    learning inquirer

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
+const inquirer = require('inquirer')
 const fs = require('fs')
-const studentName = process.argv[2]
 
-if(studentName === 'print'){
-    fs.readFile('students.txt', 'utf8', (err, data) =>{
-        if(err){
-            //throw err //if error throw to console and stop program
-            return console.log(err)
-        }
-    
-        console.log('Student List:', '\n', '----------')
-        const names = data.split('\n')
-    
-        for(let name of names){
-            console.log(name)
-        }
-    })
-}else{
-    fs.appendFile('students.txt', '\n' + studentName, (err) =>{ //will overwrite a files contents
-        if(err){
-            //throw err //if error throw to console and stop program
-            return console.log(err)
-        }
-        console.log('Student Added')
-    })
+function generateHTML(ans){
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>${ans.name}</h1>
+    <p>Fav Color: ${ans.color}</p>
+    <p>Address: ${ans.address}</p>
+</body>
+</html>
+`
+fs.writeFile('./index.html', html, (err) =>{
+    if(err){
+        return console.log(err)
+    }   
+
+    console.log('HTML file created')
+})
 }
 
+    inquirer.prompt([
+        {
+            name:'color',
+            message:'What is your favorite color?'
+        },
 
+        {
+            name:'name',
+            message: 'Please type your name'
+        },
+        {
+            name:'address',
+            message: "Please type your address"
+        }
+    ])
+        .then((answerObj) => {
+           generateHTML(answerObj)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 
-// fs.readFile('students.txt', 'utf8', (err, data) =>{
-//     if(err){
-//         //throw err //if error throw to console and stop program
-//         return console.log(err)
-//     }
-
-//     console.log('Student List:', '\n', '----------')
-//     const names = data.split('\n')
-
-//     for(let name of names){
-//         console.log(name)
-//     }
-// })
 
