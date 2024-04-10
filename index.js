df8038ce2eb22b4822f2e0c430bb712374a303ec
ThStranick <ThStranick@gmail.com>
2024-04-10 12:34:59 -0400

    adding

--- index.js
+++ index.js
@@ -1,51 +1,22 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
 
-function generateHTML(ans){
-const html = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>${ans.name}</h1>
-    <p>Fav Color: ${ans.color}</p>
-    <p>Address: ${ans.address}</p>
-</body>
-</html>
-`
-fs.writeFile('./index.html', html, (err) =>{
-    if(err){
-        return console.log(err)
-    }   
-
-    console.log('HTML file created')
-})
+function Person(name,age, hobbies){
+    this.name = name
+    this.age = age
+    this.hobbies = hobbies
 }
 
-    inquirer.prompt([
-        {
-            name:'color',
-            message:'What is your favorite color?'
-        },
+Person.prototype.haveBirthday = function (){
+    this.age++
+    console.log('Happy Birthday')
+}
 
-        {
-            name:'name',
-            message: 'Please type your name'
-        },
-        {
-            name:'address',
-            message: "Please type your address"
-        }
-    ])
-        .then((answerObj) => {
-           generateHTML(answerObj)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+Person.prototype.printBirthday = function(){
+    console.log(`You are ${this.age} years old`)
+}
 
+Person.prototype.species = 'homosapien'
 
+const thomas =  new Person('thomas', 25, ['Coding', 'Gaming'])
+thomas.haveBirthday()
+
+thomas.printBirthday()
