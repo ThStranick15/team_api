e0ea5fe076a7c3db7529ba5a75ae3e5f18b2c858
ThStranick <ThStranick@gmail.com>
2024-04-10 13:08:20 -0400

    done

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
 
-function Person(name,age, hobbies){
-    this.name = name
-    this.age = age
-    this.hobbies = hobbies
-}
+class Person{
+    constructor(name,age,hobbies){
+        this.name = name
+        this.age = age
+        this.hobbies = hobbies
+    }
 
-Person.prototype.haveBirthday = function (){
-    this.age++
-    console.log('Happy Birthday')
-}
+    haveBirthday(){
+        this.age++
+        console.log('Happy Birthday',this.name)
+    }
 
-Person.prototype.printBirthday = function(){
-    console.log(`You are ${this.age} years old`)
-}
+    printBirthday(){
+        console.log(`You are ${this.age} years old`)
+    }
 
-Person.prototype.species = 'homosapien'
+    printHobbies(){
+        const arr = this.hobbies
+        arr.forEach(element => {
+            console.log(element)
+        });
+    }
+}
 
 const thomas =  new Person('thomas', 25, ['Coding', 'Gaming'])
-thomas.haveBirthday()
 
-thomas.printBirthday()
+thomas.printHobbies()
+
+// function Person(name,age, hobbies){
+//     this.name = name
+//     this.age = age
+//     this.hobbies = hobbies
+// }
+
+
+
+// Person.prototype.haveBirthday = function (){
+//     this.age++
+//     console.log('Happy Birthday')
+// }
+
+// Person.prototype.printBirthday = function(){
+//     console.log(`You are ${this.age} years old`)
+// }
+
+// Person.prototype.species = 'homosapien'
+
+
+// thomas.haveBirthday()
+
+// thomas.printBirthday()
