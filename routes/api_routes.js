b528d1ae3dbf0b09b5ea3a08575a04ddea94b85f
ThStranick <ThStranick@gmail.com>
2024-04-17 13:31:36 -0400

    add

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,18 +3,18 @@ const { v4: generateID } = require('uuid')
 const fs = require('fs/promises')
 //const data = require('../db/users.json')
 
-async function getData(){
+async function getData() {
     const data = await fs.readFile('./db/users.json', 'utf8')
 
     return JSON.parse(data)
 }
 
 //GET all users
-router.get('/users', async (req,res) => {
+router.get('/users', async (req, res) => {
     const data = await getData()
     const nameQuery = req.query.name?.toLowerCase()
 
-    if(nameQuery){
+    if (nameQuery) {
         const user = data.find(uObj => uObj.name.toLowerCase() === nameQuery)
         return res.json(user)
     }
@@ -23,27 +23,21 @@ router.get('/users', async (req,res) => {
 })
 
 //get user by id
-router.get('/users/:user_id', async (req,res) => {
+router.get('/users/:user_id', async (req, res) => {
     const data = await getData()
     const id = req.params.user_id
-    
+
     res.json(data.find((el) => {
-        if(el.id == id || el.name == id)return true
-    }) || {message: 'User not found'})
+        if (el.id == id || el.name == id) return true
+    }) || { message: 'User not found' })
 
 })
 
-//make a POST route
-router.post('/users/form', (req,res) => {
-    console.log(req.body)
-
-    res.redirect('/')
-})
 //JS post request
-router.post('/users', async (req,res) => {
+router.post('/users', async (req, res) => {
     const id = generateID()
     const data = await getData()
-    if(!data.find((el) => el.name === req.body.name)){
+    if (!data.find((el) => el.name === req.body.name)) {
         data.push({
             ...req.body,
             id: id
@@ -55,33 +49,52 @@ router.post('/users', async (req,res) => {
             message: 'User added'
         })
     }
-    else{
+    else {
         res.json({
             message: 'User NOT added'
         })
     }
 })
 
-//JS Delete Req
-router.delete('/users', async (req,res) => {
+//JS Delete Req w/ json
+// router.delete('/users', async (req, res) => {
+//     const data = await getData()
+//     const delID = req.body.id
+//     const delEl = data.find((el) => el.id == delID)
+//     if (delEl) {
+//         const index = data.indexOf(delEl)
+//         console.log(index)
+//         data.splice(index, 1)
+//         console.log(data)
+//         await fs.writeFile('./db/users.json', JSON.stringify(data, null, 2))
+//         res.json({
+//             message: 'User deleted'
+//         })
+//     }
+//     else {
+//         res.json({
+//             message: 'User NOT deleted'
+//         })
+//     }
+
+// })
+
+//using url
+router.delete('/users/:id', async (req, res) => {
     const data = await getData()
-    const delID = req.body.id
-    const delEl = data.find((el) => el.id == delID)
-    if(delEl){
-    const index = data.indexOf(delEl)
-    console.log(index)
-    data.splice(index, 1)
-    console.log(data)
-    await fs.writeFile('./db/users.json', JSON.stringify(data, null, 2))
-    res.json({
-        message: 'User deleted'
-    })
-    }
-    else{
+    const delID = req.params.id
+    const filtered = data.filter((uObj) => uObj.id != delID) //gives back new array
+    if(data.length > filtered.length){
+        await fs.writeFile('./db/users.json', JSON.stringify(filtered, null, 2))
         res.json({
-            message: 'User NOT deleted'
+            message: `User ${delID} deleted`
         })
+        return
     }
+
+    res.json({
+        message: `User not found`
+    })
 
 })
 
