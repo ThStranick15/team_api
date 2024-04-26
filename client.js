fc8a63015412ab002de0e4d771ced410e8fb4827
ThStranick <ThStranick@gmail.com>
2024-04-26 14:08:25 -0400

    edit connection

--- client.js
+++ client.js
@@ -1,13 +1,16 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize')
 const is_prod = process.env.NODE_ENV
 
-const client = is_prod ? new Sequelize(process.env.DATABASE_URL) : new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USERNAME, 
+const client = is_prod ? new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false
+}) : new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
   process.env.DB_PASSWORD, {
   host: 'localhost',
-  dialect:  'postgres',
+  dialect: 'postgres',
   logging: false
 });
 
-  module.exports = client
+module.exports = client
