e9ad87f24715c7b8b25fe1d26208a6bad3685346
ThStranick <ThStranick@gmail.com>
2024-04-29 10:18:09 -0400

    heroku fix

--- client.js
+++ client.js
@@ -2,8 +2,12 @@ const { Sequelize } = require('sequelize')
 const is_prod = process.env.NODE_ENV
 
 const client = is_prod ? new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  logging: false
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 }) : new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
